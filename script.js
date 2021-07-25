19489918e4b98a80b2615a13681349bc2f21501b
daniel100262 <daniel100262@hotmail.com>
2021-07-25 12:17:02 -0300

    Finalizadas verificacoes

--- script.js
+++ script.js
@@ -19,8 +19,6 @@ btnSave.addEventListener("click", function () {
     newTask.taskName = txtTaskName.value;
     newTask.checked = false;
 
-    console.log("Salvando obj: " + newTask.taskName);
-
     addTaskToList(newTask, true);
 });
 
@@ -29,42 +27,44 @@ loadTaskListFromLocalStorage();
 
 function addTaskToList(newOrExistentTask, isNewTask) {
 
-    //console.log("Nome carregado: " + newOrExistentTask.taskName);
-
+    //Criando elementos
     var label = document.createElement("label");
     var checkbox = document.createElement("input");
-    checkbox.setAttribute('class', 'form-check-input me-2');
     var taskDescription = document.createTextNode(newOrExistentTask.taskName);
-    var divisoria = document.createElement("hr");
-    
+    var divisionLine = document.createElement("hr");
     var deleteButton = document.createElement("button");
+    
+    //Customizacoes
     deleteButton.setAttribute('class', 'fa fa-trash btnApagar');
-
-    var lineBreak = document.createElement("br");
+    deleteButton.setAttribute('data-toggle', 'modal');
+    deleteButton.setAttribute('data-target', '#exampleModal');
+    checkbox.setAttribute('class', 'form-check-input me-2');
 
     if(newOrExistentTask.checked){
-        console.log("1 deles ")
         label.style.textDecoration = "line-through";
         checkbox.setAttribute('checked', true);
     }
     deleteButton.setAttribute('id', newOrExistentTask.id);
 
     deleteButton.addEventListener('click', function () {
-        var div = this.parentElement;
-        div.remove();
-        apagarTarefa(newOrExistentTask.id);
+
+        if (confirm('Realmente deseja deletar esta tarefa?')) {
+            var taskElementItem = this.parentElement;
+            taskElementItem.remove();
+            deleteTask(newOrExistentTask.id);
+        }
     })
 
     checkbox.setAttribute('id', 'check');
 
     checkbox.addEventListener('change', function () {
         if (this.checked) {
-            var div = this.parentElement;
-            div.style.textDecoration = "line-through";
+            var taskElementItem = this.parentElement;
+            taskElementItem.style.textDecoration = "line-through";
             finalizeTask(newOrExistentTask);
         } else {
-            var div = this.parentElement;
-            div.style.textDecoration = "";
+            var taskElementItem = this.parentElement;
+            taskElementItem.style.textDecoration = "";
             unfinalizeTask(newOrExistentTask);
         }
     });
@@ -76,10 +76,8 @@ function addTaskToList(newOrExistentTask, isNewTask) {
     label.appendChild(checkbox); // add the box to the element
     label.appendChild(taskDescription);
     label.appendChild(deleteButton);
-    label.appendChild(divisoria);
-    label.appendChild(lineBreak);
+    label.appendChild(divisionLine);
     
-
     taskListElement.appendChild(label);
 
     txtTaskName.value = "";
@@ -90,11 +88,9 @@ function addTaskToList(newOrExistentTask, isNewTask) {
     if (isNewTask) { //Impede loop
         saveListToLocalStorage(itemsList);
     }
-
-
 }
 
-function apagarTarefa(taskId) {
+function deleteTask(taskId) {
 
     var localStorageTaskList = JSON.parse(localStorage.getItem("taskList"));
     var itemParaRemover;
